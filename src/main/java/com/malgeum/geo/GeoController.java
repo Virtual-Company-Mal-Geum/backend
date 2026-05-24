b5e823666b93f4cf2df3ec9294da6b3a21b7c860
송지한 <157107006+Song-Jihan@users.noreply.github.com>
2026-05-24 11:20:06 +0900

    feat)add update form to change password

--- src/main/java/com/malgeum/geo/GeoController.java
+++ src/main/java/com/malgeum/geo/GeoController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.malgeum.geo.domain.domain.ReportResult;
 import com.malgeum.geo.domain.domain.Order.CategoryStatus;
+import com.malgeum.geo.global.ClientUpdateForm;
 import com.malgeum.geo.service.AuthService;
 import com.malgeum.geo.service.OrderService;
 import com.malgeum.geo.service.ReportService;
@@ -36,6 +37,7 @@ public class GeoController {
         return ResponseEntity.ok("redirect:/home");
     }
     
+    //추출한 페이지 구조 분석 요청 (프론트->백엔드)
     @PostMapping("/analyze")
     public ResponseEntity<GeoEvaluationResponse> startAnalysis(@RequestBody GeoEvaluationRequest request) {
         Long orderId = orderService.acceptOrder(request.targetUrl(),request.categoryStatus());
@@ -43,12 +45,16 @@ public class GeoController {
                 .body(new GeoEvaluationResponse("GEO 분석 요청이 성공적으로 접수되었습니다.", orderId));
     }
 
+    //TODO:분석 완료 후, AI서버로부터 온 결과물 받고 처리 (AI서버->)
+
+    //AI 분석 결과 조회 (백엔드->프론트)
     @GetMapping("/report/{orderId}")
     public ResponseEntity<ReportResult> getReport(@PathVariable Long orderId) {
         ReportResult report = reportService.getReportDetails(orderId);
         return ResponseEntity.ok(report);
     }
 
+    //회원가입
     @GetMapping("/signup")
     public ResponseEntity<String> signUp(SignUpForm form){
         return ResponseEntity.ok("geo-signup");
@@ -60,8 +66,14 @@ public class GeoController {
         return ResponseEntity.ok().build();
     }
 
+    //로그인
     @GetMapping("/login")
     public ResponseEntity<String> login() {
         return ResponseEntity.ok("geo-login");
     }
+
+    // @GetMapping("/change_password")
+    // public ResponseEntity<String> changePassword(@RequestBody ClientUpdateForm clientUpdateForm){
+    //     return ResponseEntity.ok("geo-change_password");
+    // }
 }
