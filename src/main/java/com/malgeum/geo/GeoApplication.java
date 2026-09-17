1c61fbeafc79ccd077b05e1f7b6ab252a5213ab1
송지한 <157107006+Song-Jihan@users.noreply.github.com>
2026-09-17 17:06:01 +0900

    fix)separate config files

--- src/main/java/com/malgeum/geo/GeoApplication.java
+++ src/main/java/com/malgeum/geo/GeoApplication.java
@@ -7,9 +7,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableJpaAuditing
-@EnableAsync
-@EnableScheduling
 public class GeoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GeoApplication.class, args);
